905278172cc4e75a51f8eef3c19051ebec8f816d
zZZuyZz <duylieng.ntt@gmail.com>
2022-09-23 13:15:11 +0700

    Add DTO

--- Backup/Full/NestSA/src/java/order/OrderDTO.java
+++ Backup/Full/NestSA/src/java/order/OrderDTO.java
@@ -10,5 +10,62 @@ package order;
  * @author Admin
  */
 public class OrderDTO {
+    private int orderId;
+    private String date;
+    private String address;
+    private int userId;
+    private String phone;
+
+    public OrderDTO() {
+    }
+
+    public OrderDTO(int orderId, String date, String address, int userId, String phone) {
+        this.orderId = orderId;
+        this.date = date;
+        this.address = address;
+        this.userId = userId;
+        this.phone = phone;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
     
 }
